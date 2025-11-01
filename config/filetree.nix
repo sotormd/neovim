0fa20029d1ea0b997426c6155f4d58af7c36756a
vrm <sotormd@proton.me>
2025-11-02 00:28:34 +0530

    feat: add neotree toggle keybind

--- config/filetree.nix
+++ config/filetree.nix
@@ -4,4 +4,7 @@
     # enable neo-tree, a modern replacement for nvim-tree
     neo-tree.enable = true;
   };
+  config.vim.luaConfigPost = ''
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+  '';
 }
