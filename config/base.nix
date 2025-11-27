6ab765afd39790cf1ec485d4c4f0c1174f85f1f8
vrm <sotormd@proton.me>
2025-11-28 00:30:57 +0530

    fix: add colors.neovim support for lualine

--- config/base.nix
+++ config/base.nix
@@ -1,9 +1,11 @@
 # statusline and bufferline configuration
+{ colors, ... }:
+
 {
   config.vim = {
     statusline.lualine = {
       enable = true; # enable modern statusline
-      theme = "nord"; # match overall color theme
+      theme = colors.neovim.name; # match overall color theme
     };
 
     tabline.nvimBufferline.enable = true; # better buffer tabs with numbers
