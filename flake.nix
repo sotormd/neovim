d2eba97f5da1d50b7d4c9a1e449c2a4cbd074b89
vrm <sotormd@proton.me>
2026-05-01 00:16:17 +0530

    feat: pin nvf

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
+    nvf.inputs.nixpkgs.follows = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     colors.url = "github:sotormd/colors";
   };
